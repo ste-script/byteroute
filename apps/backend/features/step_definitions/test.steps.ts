756a106cb828632e5063ea6a31ee0d078f34b2af
Stefano Babini <ste10babini@gmail.com>
2026-02-20 11:04:46 +0000

    test(backend): setup gherkin

---
+++ apps/backend/features/step_definitions/test.steps.ts
@@ -0,0 +1,14 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+
+Given('I have a test scenario', function () {
+  // Write code here that turns the phrase above into concrete actions
+});
+
+When('I run the test', function () {
+  // Write code here that turns the phrase above into concrete actions
+});
+
+Then('I should see the expected results', function () {
+  // Write code here that turns the phrase above into concrete actions
+});
+
---
+++ apps/backend/features/test.feature
@@ -0,0 +1,6 @@
+Feature: test feature
+
+  Scenario: test scenario
+    Given I have a test scenario
+    When I run the test
+    Then I should see the expected results
